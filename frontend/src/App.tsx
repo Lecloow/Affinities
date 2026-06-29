4c22ca7ca7faae5229189bb56c3a641728b21e26
Thomas Conchon <150849104+Lecloow@users.noreply.github.com>
2026-06-29 21:06:51 +0200

    feat: add admin panel to website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Leaderboard from "./pages/leaderboard";
+import { Documentation } from "@/components";
 
 export default function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/leaderboard" element={<Leaderboard/>} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/leaderboard" element={<Leaderboard />} />
+      <Route path="/admin-panel" element={<Documentation />} />
+    </Routes>
   );
 }
---
+++ frontend/src/components/Documentation.tsx
@@ -0,0 +1,13 @@
+export default function Documentation() {
+  return (
+    <iframe
+      src="/Affinities-documentation.html"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+      }}
+      title="Documentation"
+    />
+  );
+}
